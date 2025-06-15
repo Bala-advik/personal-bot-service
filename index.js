f8b0e26464f384808f84f2fc8f49b5d0678f142e
Bala-Advik <advik15andydev@gmail.com>
2025-06-15 13:17:09 +0530

    LLM Feature Added

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const express = require("express");
-const TelegramBot = require("node-telegram-bot-api");
+import express from "express";
+import TelegramBot from "node-telegram-bot-api";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { PromptTemplate } from "@langchain/core/prompts";
+import { StringOutputParser } from "@langchain/core/output_parsers";
 require("dotenv").config();
 
 const app = express();
@@ -12,19 +15,42 @@ const bot = new TelegramBot(token, { polling: true });
 // Bot command: /start
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(
-    chatId,
-    "Welcome to MyExpressBot! Send any message, and I’ll echo it back."
-  );
+  bot.sendMessage(chatId, "It's Adviks Bot, Send a Message");
 });
 
 // Echo any text message
-bot.on("message", (msg) => {
+bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
   if (!text.startsWith("/")) {
-    // Ignore commands
-    bot.sendMessage(chatId, `You said: ${text}`);
+    try {
+      const question = msg.text;
+      const modelConfig = {
+        apiKey: process.env.API_KEY,
+        model: "gemma-3-4b-it",
+      };
+
+      const model = new ChatGoogleGenerativeAI(modelConfig);
+
+      const prompt = new PromptTemplate({
+        inputVariables: ["question"],
+        inputVariables: ["question"],
+        template: `You are a deep search robot who loves sharing up to date details!.Now answer this question: {question} Make it return updated results.`,
+      });
+
+      const formattedPrompt = await prompt.format({
+        question,
+      });
+
+      const response = await model.invoke(formattedPrompt);
+      const parsedResponse = await new StringOutputParser().parse(
+        response.content
+      );
+
+      bot.sendMessage(chatId, parsedResponse);
+    } catch (error) {
+      console.error("Robot got confused:", error.message);
+    }
   }
 });
 
